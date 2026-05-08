da81676f3d6c3ce107ae4ba5080f3654893c0e7e
Piash Islam <piashislam2k@gmail.com>
2026-05-08 22:50:52 +0600

    deman discovery section added done

---
+++ src/components/DemandDiscoverySection.jsx
@@ -0,0 +1,130 @@
+export default function DemandDiscoverySection() {
+  return (
+    <section className="w-full py-12 | xl:py-24">
+
+      <div className="w-full px-4 | md:px-7">
+
+        <div className="w-full flex justify-between items-start | flex-col-reverse | md:flex-row | gap-x-3 | md:gap-x-5 gap-y-3 | md:gap-y-5">
+
+          <div className="flex flex-wrap gap-4 w-full | md:hidden">
+
+            <a href="https://riseatseven.com/about/" target="" className="w-full group inline-flex shrink-0 justify-center gap-x-2 items-center relative leading-tight tracking-tightish capitalize font-sans-primary! font-medium overflow-hidden border border-transparent cursor-pointer | focus:outline-none | md:w-auto text-base px-6 py-3 rounded-3xl transition transition-rounded | focus:ring-3 | pointer-fine:hover:rounded-xl bg-white text-grey-900 ring-grey-900/5 flex-row-reverse">
+              <div className="relative overflow-hidden">
+                <div className="transition | pointer-fine:group-hover:-translate-y-6">
+                  <div className="flex items-center gap-x-2">
+                    <span>Our Story</span>
+                    <span className="inline-block align-middle motion-safe:transition text-xs mt-1" aria-hidden="true">
+                      <i className="fa-regular fa-sharp fa-arrow-up-right" aria-hidden="true"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="transition absolute top-0 left-0 translate-y-6 | pointer-fine:group-hover:translate-y-0">
+                  <div className="flex items-center gap-x-2">
+                    <span>Our Story</span>
+                    <span className="inline-block align-middle motion-safe:transition text-xs mt-1" aria-hidden="true">
+                      <i className="fa-regular fa-sharp fa-arrow-up-right" aria-hidden="true"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+            <a href="https://riseatseven.com/services/" target="" className="w-full group inline-flex shrink-0 justify-center gap-x-2 items-center relative leading-tight tracking-tightish capitalize font-sans-primary! font-medium overflow-hidden border border-transparent cursor-pointer | focus:outline-none | md:w-auto text-base bg-transparent text-grey-900 flex-row-reverse">
+              <div className="relative overflow-hidden">
+                <div className="transition | pointer-fine:group-hover:-translate-y-6">
+                  <div className="flex items-center gap-x-2">
+                    <span>Our Services</span>
+                    <span className="inline-block align-middle motion-safe:transition text-xs mt-1" aria-hidden="true">
+                      <i className="fa-regular fa-sharp fa-arrow-up-right" aria-hidden="true"></i>
+                    </span>
+                  </div>
+                </div>
+                <div className="transition absolute top-0 left-0 translate-y-6 | pointer-fine:group-hover:translate-y-0">
+                  <div className="flex items-center gap-x-2">
+                    <span>Our Services</span>
+                    <span className="inline-block align-middle motion-safe:transition text-xs mt-1" aria-hidden="true">
+                      <i className="fa-regular fa-sharp fa-arrow-up-right" aria-hidden="true"></i>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </a>
+
+          </div>
+
+          <div className="w-full mb-1 | md:mt-2 md:mb-0 max-w-sm | xl:max-w-xl | 3xl:max-w-2xl | 4xl:max-w-3xl">
+            <div className="inline-flex flex-wrap text-balance relative text-left justify-start text-grey-900 text-lg/tight | lg:text-lg/tight | xl:text-2xl/none | 4xl:text-3xl/none font-sans-primary font-medium tracking-tight js-heading">A global team of search-first content marketers engineering semantic relevancy &amp; category signals for both the internet and people</div>
+          </div>
+
+          <div className="w-full grid | max-w-96 | md:max-w-160 | xl:max-w-xl | 2xl:max-w-2xl | 3xl:max-w-208 | 4xl:max-w-5xl | gap-y-3 | md:gap-y-7">
+
+            <h2 className="inline-flex flex-col text-left justify-start text-grey-900 text-5xl/none | lg:text-6xl/none | xl:text-7xl/0.9 | 3xl:text-7.5xl/0.9 | 4xl:text-8xl/0.9 font-sans-primary font-medium tracking-tight js-heading">
+              <div className="flex flex-wrap relative text-left justify-start">
+                <div className="inline mr-2 | pointer-fine:mr-0 | js-word" style={{marginRight:14}}>Driving</div>
+                <div className="inline mr-2 | pointer-fine:mr-0 | js-word" style={{marginRight:14}}>Demand</div>
+                <div className="inline mr-2 | pointer-fine:mr-0 | js-word" style={{marginRight:14}}>&amp;</div>
+                <div className="inline mr-2 | pointer-fine:mr-0 | js-word" style={{marginRight:14}}>Discovery</div>
+                <div className="inline-flex shrink-0 bg-black/5 relative overflow-hidden mr-2 | pointer-fine:mr-0 | js-image-wrapper" style={{borderRadius:'15%', width:90, marginRight:14}}>
+                  <div className="w-full h-full relative | js-image">
+                    <picture>
+                      <source type="image/webp" srcSet="https://rise-atseven.transforms.svdcdn.com/production/images/b2087e0cd3f699d3efc76f809ec72a85a6ab378e-1080x1350.jpg?w=200&amp;h=200&amp;q=80&amp;fm=webp&amp;fit=crop&amp;crop=focalpoint&amp;fp-x=0.5&amp;fp-y=0.5&amp;dm=1750847630&amp;s=a668733e8ced1733809794da9c15f062 200w" sizes="100vw" />
+                      <img src="https://rise-atseven.transforms.svdcdn.com/production/images/b2087e0cd3f699d3efc76f809ec72a85a6ab378e-1080x1350.jpg?w=200&amp;h=200&amp;q=80&amp;fm=webp&amp;fit=crop&amp;crop=focalpoint&amp;fp-x=0.5&amp;fp-y=0.5&amp;dm=1750847630&amp;s=a668733e8ced1733809794da9c15f062" srcSet="https://rise-atseven.transforms.svdcdn.com/production/images/b2087e0cd3f699d3efc76f809ec72a85a6ab378e-1080x1350.jpg?w=200&amp;h=200&amp;q=90&amp;auto=format&amp;fit=crop&amp;crop=focalpoint&amp;fp-x=0.5&amp;fp-y=0.5&amp;dm=1750847630&amp;s=2cf16743338412726c0b8ad40904233f 200w" sizes="100vw" alt="B2087e0cd3f699d3efc76f809ec72a85a6ab378e 1080x1350" className=" w-full h-full object-cover object-center absolute inset-0" />
+                    </picture>
+                  </div>
+                </div>
+              </div>
+            </h2>
+
+            <div className="hidden | md:flex flex-wrap gap-4">
+              <a href="https://riseatseven.com/about/" target="" className="w-full group inline-flex shrink-0 justify-center gap-x-2 items-center relative leading-tight tracking-tightish capitalize font-sans-primary! font-medium overflow-hidden border border-transparent cursor-pointer | focus:outline-none | md:w-auto text-base px-6 py-3 rounded-3xl transition transition-rounded | focus:ring-3 | pointer-fine:hover:rounded-xl bg-white text-grey-900 ring-grey-900/5 flex-row-reverse">
+                <div className="relative overflow-hidden">
+                  <div className="transition | pointer-fine:group-hover:-translate-y-6">
+                    <div className="flex items-center gap-x-2">
+                      <span>Our Story</span>
+                      <span className="inline-block align-middle motion-safe:transition text-xs mt-1" aria-hidden="true">
+                        <i className="fa-regular fa-sharp fa-arrow-up-right" aria-hidden="true"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="transition absolute top-0 left-0 translate-y-6 | pointer-fine:group-hover:translate-y-0">
+                    <div className="flex items-center gap-x-2">
+                      <span>Our Story</span>
+                      <span className="inline-block align-middle motion-safe:transition text-xs mt-1" aria-hidden="true">
+                        <i className="fa-regular fa-sharp fa-arrow-up-right" aria-hidden="true"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </a>
+
+              <a href="https://riseatseven.com/services/" target="" className="w-full group inline-flex shrink-0 justify-center gap-x-2 items-center relative leading-tight tracking-tightish capitalize font-sans-primary! font-medium overflow-hidden border border-transparent cursor-pointer | focus:outline-none | md:w-auto text-base bg-transparent text-grey-900 flex-row-reverse">
+                <div className="relative overflow-hidden">
+                  <div className="transition | pointer-fine:group-hover:-translate-y-6">
+                    <div className="flex items-center gap-x-2">
+                      <span>Our Services</span>
+                      <span className="inline-block align-middle motion-safe:transition text-xs mt-1" aria-hidden="true">
+                        <i className="fa-regular fa-sharp fa-arrow-up-right" aria-hidden="true"></i>
+                      </span>
+                    </div>
+                  </div>
+                  <div className="transition absolute top-0 left-0 translate-y-6 | pointer-fine:group-hover:translate-y-0">
+                    <div className="flex items-center gap-x-2">
+                      <span>Our Services</span>
+                      <span className="inline-block align-middle motion-safe:transition text-xs mt-1" aria-hidden="true">
+                        <i className="fa-regular fa-sharp fa-arrow-up-right" aria-hidden="true"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import ClientLogoMarquee from "../components/ClientLogoMarquee";
+import DemandDiscoverySection from "../components/DemandDiscoverySection";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <Hero />
+      <Hero/>
       <ClientLogoMarquee />
+      <DemandDiscoverySection />
     </>
   );
 }
